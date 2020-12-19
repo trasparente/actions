0e909d74766422a19246bc446489bae01f7b5604
Claudio Sereti <stradatrasparente@gmail.com>
2020-12-19 17:26:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 module.exports = ({github, context, files}) => {
   console.log('from inside', files.data);
-  const files = github.pulls.listFiles({
+  const files_double = github.pulls.listFiles({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.number
