17c5ab88d8bbab97965315ea68a076f52a82b7dc
Claudio Sereti <stradatrasparente@gmail.com>
2020-12-19 15:52:02 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 module.exports = ({github, context}) => {
-  return context.payload.client_payload.value
+  return {context.payload.number, contenxt.payload.number, github.event.number, "https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}/files"};
 }
