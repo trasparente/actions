f94378bac852e4ff85d1406eeb5dd882913c1167
Claudio Sereti <stradatrasparente@gmail.com>
2020-12-19 18:27:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 module.exports = ({github, context}) => {
+  const get_pull = (response) => {
+    console.log([response.data.filename, response.data.contents_url]);
+    return true;
+  };
   const files_double = github.pulls.listFiles({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.number
-  }).then((response) => {
-    return [response.data.filename, response.data.contents_url];
-  });
+  }).then(get_pull);
 }
