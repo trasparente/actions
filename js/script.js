b3cd1b207af7ac82c93454460c76d3f778b047fb
Claudio Sereti <stradatrasparente@gmail.com>
2020-12-23 16:24:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 module.exports = ({github, context}) => {
-  const listFiles = github.pulls.listFiles({
+  // List pull request files
+  github.pulls.listFiles({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.number
-  });
-  const loop = listFiles.then((response) => {
-    const output = response.data.map((file) => {
-      console.log(file);
-      return file;
+  }).then((response) => {
+    // Loop files and get content
+    return response.data.map((file) => {
+      const content = github.request("GET " + file.contents_url);
+      return content.data;
     });
-    return output;
-  });
-  loop.then((array) => {
-    console.log("final", array.length);
+  }).then((array) => {
+    // log contents array
+    console.log("final", array);
   });
   return true;
 }
