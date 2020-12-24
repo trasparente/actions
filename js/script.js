37eacb454231de555b53b0d1db3945ce6da496b6
Claudio Sereti <stradatrasparente@gmail.com>
2020-12-24 19:50:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-module.exports = async ({github, context}) => {
+module.exports = ({github, context}) => {
 
   // console.log('context.actor', context.actor);
 
   // List pull request files
   // Return a promised array {filename: "_data/players/trasparente/profile.json", contents_url: "..."}
-  const listFiles = await github.pulls.listFiles({
+  const listFiles = github.pulls.listFiles({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.number
@@ -12,17 +12,17 @@ module.exports = async ({github, context}) => {
   
   // Loop files and get request contents
   // Return an array of promises {name: "profile.json", content: "..."}
-  const request_array = async (listFiles) => listFiles.data.map((file) => await github.request(`GET ${file.contents_url}`))
+  const request_array = listFiles => listFiles.data.map(file => github.request(`GET ${file.contents_url}`))
 
   // CHAIN PROMISES
   // Execute in parallel and wait all, return array of contents objects
-  const chain = async (array) => await Promise.all(array)
+  const chain = array => Promise.all(array)
 
   // LOG
   // All promises resolved, checkpoint
-  const log = contents => contents.map(content => [content.data.name, content.data.path, JSON.parse(Buffer.from(content.data.content, 'base64').toString('binary'))])
+  const log = contents => contents.map(content => console.log(content.data.name, content.data.path, JSON.parse(Buffer.from(content.data.content, 'base64').toString('binary'))))
 
   // RETURN MODULE
-  console.log(log());
-  return log();
+  listFiles.then(request_array).then(chain).then(log)
+  return true
 })
